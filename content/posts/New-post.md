051764fc83bf68d5d92baf114a10f06e11b8d5cc
Macbook Pro <macbookpro@macbooks-mbp.mynetworksettings.com>
2023-04-25 14:45:21 -0700

    first commit

---
+++ content/posts/New-post.md
@@ -0,0 +1,5 @@
+---
+title: New post
+---
+
+ssdfsdf;ljkdfg
---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "db449158-097f-42d5-b27a-17f6833ee0bd", // Get this from tina.io
+  token: "7166608c082cd590f59cf30bed570cbde59fc59c", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "./",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "./",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
